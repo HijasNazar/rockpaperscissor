d59acbff61074cde99c18bcbf5ad4a70179edaf0
hijas <hijasnazar7@gmail.com>
2021-08-14 17:23:51 +0530

    Add index.html

--- external.js
+++ external.js
@@ -1,113 +1,126 @@
 "use strict";
+let text=document.querySelector("#text");
+let sctarr=Array.from(document.querySelectorAll('button'));
+sctarr.forEach(function(element){
+    
+    element.addEventListener('click',function()
+    {
+        let tex;
+        tex=element.id;
+        playRound(tex);
+    })
+})
+
 function computerPlay(){
     const inp = ["rock", "paper", "scissor"];
     let ran=Math.floor(Math.random() * 3);
     return inp[ran]
 }
 
-function game(playerSelection,computerSelection){
+let nthround=1;
+let finalplayer=0;
+let finalcomp=0;
+
+
+
+
+
+function playRound(playerSelection){
     let playermarks,compmarks;
-
-if(playerSelection==computerSelection)
-{
-    playermarks=0;
-    compmarks=0;
-}
-
-else if(playerSelection=="rock")
-{
-    if(computerSelection=="paper")
-    {
-        compmarks=1;
-        playermarks=0;
-
-
+    
+    if(nthround>5){
+        console.log("entered");
+        textner(finalplayer,finalcomp);
     }
+
+    if(nthround<=5)
+    {
+    
+    let computerSelection=computerPlay();
+    console.log("round:",nthround,"player:", finalplayer,"computer:", finalcomp);
+
+    if(playerSelection==computerSelection)
+    {   
+        playermarks=0; compmarks=0;
+        console.log(playermarks,compmarks);
+    }
+
+    else if(playerSelection=="rock")
+    {
+        if(computerSelection=="paper")
+    {
+        compmarks=1; playermarks=0;
+        console.log(playermarks,compmarks);
+    }
+
     else if(computerSelection=="scissor")
     {
-        playermarks=1;
-        compmarks=0;
-
+        playermarks=1; compmarks=0;
+        console.log(playermarks,compmarks);
     }
-}
+    }
 
-else if(playerSelection=="paper")
-{
-    if(computerSelection=="scissor")
+    else if(playerSelection=="paper")
     {
-        compmarks=1;
-        playermarks=0;
-
-
+        if(computerSelection=="scissor")
+    {
+        compmarks=1; playermarks=0;
+        console.log(playermarks,compmarks);
     }
     else if(computerSelection=="rock")
     {
-        playermarks=1;
-        compmarks=0;
-
+        playermarks=1; compmarks=0;
+        console.log(playermarks,compmarks);
     }
-}
+    }
 
-else if(playerSelection=="scissor")
-{
+    else if(playerSelection=="scissor")
+    {
     if(computerSelection=="rock")
     {
-        compmarks=1;
-        playermarks=0;
-
-
+        compmarks=1;playermarks=0;
+        console.log(playermarks,compmarks);
     }
     else if(computerSelection=="paper")
     {
-        playermarks=1;
-        compmarks=0;
-
-
+        playermarks=1; compmarks=0;
+        console.log(playermarks,compmarks);
     }
-}
-
-return [playermarks,compmarks];
-
-}
-let comp=0;
-let player=0;
-
-function actualgame(roundsnumber){
-    for(let i=0;i<roundsnumber;i++)
-    {
-        
-        let compinp=computerPlay();
-        let userinp = window.prompt("Input (Give rock/paper/scissor");
-        let userinp1=userinp.toLowerCase();
-        console.log(`Round ${i}`);
-        console.log("Computer chose " +compinp+" and you chose " +userinp1);
-        let out=game(userinp1,compinp);
-        comp=comp+out[0];
-        player=player+out[1];
-        console.log(`User points:${comp} Computer points:${player}`);
     }
+    const div = document.createElement(`div${1}`);
+    document.querySelector("#roundno").textContent=`ROUND ${nthround}`;
+    document.querySelector("#text").textContent=`ROUND ${nthround}: Player selected ${playerSelection} and computer selected ${computerSelection}. Player scored:${playermarks} Computer scored:${compmarks}`;
 }
-function winner(num1,num2){
+    else {
+    nthround++;
+    console.log('nth',nthround);
+    return;
+    }
+    finalplayer=finalplayer+playermarks;
+    finalcomp=finalcomp+compmarks;
+    console.log("final",finalplayer,finalcomp);
+    nthround++;
+    return [finalplayer,finalcomp];
+}
+
+function textner(num1,num2){
     if(num1>num2)
     {
-        console.log("YOU WON AGAINST COMPUTER");
-        return "YOU WON AGAINST COMPUTER";
+        document.querySelector("#text").textContent="YOU WON AGAINST COMPUTER";
+        document.body.style.backgroundColor = "green";
+        return
     }
 
     else if(num2>num1)
     {
-            console.log("COMPUTER WON AGAINST YOU");
-            return "COMPUTER WON AGAINST YOU";
+        document.querySelector("#text").textContent="COMPUTER WON AGAINST YOU";
+        document.body.style.backgroundColor = "blue";
+        return
     }
     
     else if(num1==num2)
     {
-        console.log("TIE");
+        document.querySelector("#text").textContent="TIE";
+        return
     }
 }
-
-let rounds = window.prompt("NUMBER OF ROUNDS");
-let result=actualgame(rounds);
-let win=winner(player,comp);
-console.log(typeof(win));
-document.write(win);
